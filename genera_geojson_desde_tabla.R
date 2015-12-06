2c9c0be73872002e4353264463e832cf440f1b7a
brolin <brolin108@gmail.com>
2015-12-06 15:26:54 -0500

    Se corrige la función flat_data para que devuelva también las coordenadas

--- genera_geojson_desde_tabla.R
+++ genera_geojson_desde_tabla.R
@@ -1,26 +1,28 @@
 rm(list=ls())
 
 ## require(devtools)
+## Paquete dpmr data package manager para R no funciona
 ## install_github("christophergandrud/dpmr")
-require(dpmr) ## https://github.com/christophergandrud/dpmr/
-URL <- "https://github.com/son0p/dataSet_grupos_casa_teatro/archive/master.zip"
-gdp_data <- datapackage_install(path = URL)
-str(gdp_data)
+## require(dpmr) ## https://github.com/christophergandrud/dpmr/
+## URL <- "https://github.com/son0p/dataSet_grupos_casa_teatro/archive/master.zip"
+## gdp_data <- datapackage_install(path = URL)
+## str(gdp_data)
 ###  No funciona tan bien el paquete de R ###
 
-require(RCurl)
-require(XML)
-require(stringr)
-require(stringi)
-direccion <- "https://raw.githubusercontent.com/son0p/dataSet_grupos_casa_teatro/master/data/Artistas_casa_teatro_filtrado.csv"
-url_direccion<- getURL(direccion)
-data <- read.csv(text = url_direccion)
+require(RCurl) ## paquete interfaz para curl
+require(stringr) ## paquete para manipulación de strings
+require(stringi) ## paquete para manipulación de strings
 
-## El dato de la coordenada viene en el campo geometry pero viene en formato xml dentro de Point > coordinates, se usa el siguiente patrón para quitar lo que sobra y dejar solo latitud y longitud al extraérselo como texto
+direccion <- "https://raw.githubusercontent.com/son0p/dataSet_grupos_casa_teatro/master/data/Artistas_casa_teatro_filtrado.csv"
+url_direccion<- getURL(direccion) ## Se trae la página inidcada en direccion en el formato csv
+data <- read.csv(text = url_direccion) ## Se lee el csv obtenido de internet y se convierte en tabla para guardarlo en data
+
+## El dato de la coordenada viene en el campo geometry pero viene en formato xml dentro de Point > coordinates, se usa el siguiente patrón de expresión regular para quitar lo que sobra y dejar solo latitud y longitud al extraérselo como texto
 pattern <- "-.[0-9].[0-9]+,[0-9].[0-9]+"
 data$geometry <- str_extract(data$geometry, pattern)
 data
 
+## Creo una plantilla de un punto en geojson, dejan los valores nombre y coordenada para reemplazar desde la tabla
 plantilla <-  '{
   "type": "FeatureCollection",
   "features": [
@@ -39,11 +41,10 @@ plantilla <-  '{
   ]
 }'
 
-plantilla <- str_replace(plantilla,"nombre",data$name)
-plantilla <- str_replace(plantilla,"coordenada",data$geometry)
+plantilla <- str_replace(plantilla,"nombre",data$name) # Se reemplaza el nombre en la plantilla
+plantilla <- str_replace(plantilla,"coordenada",data$geometry) # Se reemplaza la coordenada en la plantilla
 
-stri_trans_list()
-
+## Función para normalizar los nombres de las agrupaciones de modo que no tengan caracteres raros y los espacios sean reemplazados por guión bajo
 normalizarNombre <- function(nombre) {
     nombre <- str_replace_all(nombre,"X","")
     nombre <- str_replace_all(nombre,"[[:punct:]]"," ")
@@ -53,7 +54,7 @@ normalizarNombre <- function(nombre) {
     nombre
 }
 
-
+## Se recorren todas las plantillas, se normaliza el nombre y se guardan en el sistema con el nombre de la agrupación más la extensión .geojson
 for(i in 1:length(plantilla)) {
     nombre <- normalizarNombre(data$name[i])
     cat(plantilla[i], file = paste0("mapasGrupos/",nombre,".geojson"))
--- visualizacion_apariciones.R
+++ visualizacion_apariciones.R
@@ -2,43 +2,69 @@ rm(list=ls())
 
 require(plyr)
 require(jsonlite)
-
-library(datacomb)
+require(stringr)
+require(stringi)
 
 grupos <- c("providencia","de_bruces_a_mi", "tarmac")
 
-fromJSON("https://raw.githubusercontent.com/son0p/mapasGrupos/master/athemesis.geojson")
-
+## Del repositorio son0p/mapasGrupos retorna los enlaces a los archivos geojson
 get_raw_link <- function(github_repo, grupo) {
     paste0("https://raw.githubusercontent.com/",github_repo,"/master/",grupo,".geojson")
 }
 
+## Del repositorio son0p/mapasGrupos retorna los archivos geojson
 get_raw_geojson <- function(github_repo, grupo) {
     tryCatch({
-        geojson <- fromJSON(paste0("https://raw.githubusercontent.com/",github_repo,"/master/",grupo,".geojson"))
+        geojson <- fromJSON(get_raw_link(github_repo,grupo))
         geojson
     }, error = function(e) {print("error")})
+}
+
+## Basado en la estructura del geojson toma las features y sus propiedades y los exporta como data.frame
+## Acá solo cojo los datos de las features y no la geometría que contiene las coordenadas
+flat_data <- function(mapas) {
+    flatten_data <- lapply(setNames(mapas, names(mapas)), function(x) cbind2(x[[2]][[2]], x[[2]][[3]]))
+    ldply(flatten_data, cbind)
+}
+
+open_in_excel <- function(some_df){
+    tFile<-tempfile(fileext=paste0(substitute(some_df), ".csv"),tmpdir="/tmp")
+    write.csv2(some_df, tFile)
+    system(paste('libreoffice ', tFile, '&'))
+}
+
+normalizarNombre <- function(nombre) {
+    nombre <- str_replace_all(nombre,"X","")
+    nombre <- str_replace_all(nombre,"[[:punct:]]"," ")
+    nombre <- stri_trans_general(nombre, "Any-Title")
+    nombre <- str_replace_all(nombre,"\\s","")
+    nombre <- stri_trans_general(nombre, "latin-ascii")
+    nombre
 }
 
 mapas <- lapply(setNames(grupos, grupos), function(x) get_raw_geojson("son0p/mapasGrupos",x))
 
 get_raw_link("son0p/mapasGrupos", grupos)
 
-flat_data <- function(mapa) {
-    features <- mapas[[1]][[2]]
-    print((features))
-    ##cbind(names(features), features)
+flatten_data <- flat_data(mapas)
+colnames(flatten_data) <- normalizarNombre(colnames(flatten_data))
+str(flatten_data)
+
+require(timelineR)
+timeline()
+
+
+format_data <- function(data) {
+    len <- length(data)
+    types <- c()
+    for(i in 1:len) {
+        types[i] <- switch(typeof(data[,i]), character="as.factor", logical="as.factor", integer="as.numeric","")
+    }
+    (expr <- paste0(types,"(data$",colnames(data),")"))
+    for(i in 1:len){
+        data[i] <- (eval(parse(text = expr[i])))
+    }
+    data
 }
 
-flatten_data <- lapply(setNames(mapas, names(mapas)), flat_data)
-
-cbind
-
-names(mapas)
-
-names(mapas[1])
-cbind(names(mapas[1]),mapas[1]$providencia$features)
-cbind(names(mapas[2]),mapas[2]$providencia$features)
-cbind(names(mapas[2]),mapas[2]$providencia$features)
-
-str(mapas$tarmac$features)
+flatten_data <- format_data(flatten_data)
