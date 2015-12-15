d53834e9725822f3a92b8bdb07426be0270b61eb
brolin <brolin108@gmail.com>
2015-12-15 07:05:38 -0500

    Agregada gráfica streamgraph

--- visualizacion_apariciones.R
+++ visualizacion_apariciones.R
@@ -4,8 +4,9 @@ require(plyr)
 require(jsonlite)
 require(stringr)
 require(stringi)
+require(lubridate)
 
-grupos <- c("providencia","de_bruces_a_mi", "tarmac", "esteman","laura_y_la_maquina_de_escribir", "pedrina_y_el_rio")
+grupos <- c("esteman","laura_y_la_maquina_de_escribir", "pedrina_y_rio")
 
 ## Del repositorio son0p/mapasGrupos retorna los enlaces a los archivos geojson
 get_raw_link <- function(github_repo, grupo) {
@@ -56,3 +57,10 @@ colnames(flatten_data) <- normalizarNombre(colnames(flatten_data))
 str(flatten_data) # Sacar los datos de las coordenadas
 
 open_in_excel(flatten_data)
+
+flatten_data <- flatten_data[-which(is.na(flatten_data$Date)),] # Se retira donde no hay fecha
+flatten_data$Date <- ymd(flatten_data$Date)
+flatten_data$Capacity <- as.numeric(flatten_data$Capacity)
+devtools::install_github("hrbrmstr/streamgraph")
+require(streamgraph)
+streamgraph(flatten_data, "Id", "Capacity" , "Date", interactive = TRUE , interpolate="step", offset = "zero") %>%  sg_legend(show=TRUE, label="Agrupación: ") %>% sg_axis_x(20, "año", "%Y")
